1f6013fc3e5ac4eecb70caf1a910143352fc8d17
Rigmor Lindberg <56433785+RigmorLindberg@users.noreply.github.com>
2024-11-19 16:07:39 +0100

    Update ventetidsindikatoren.py

--- dags/ventetidsindikatoren.py
+++ dags/ventetidsindikatoren.py
@@ -1,14 +1,15 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
-from dataverk_airflow import notebook_operator
+from dataverk_airflow import notebook_operator, python_operator
 
 
 with DAG('ventetidsindikatoren', start_date=days_ago(1), schedule="0 8 * * 1-5", catchup=False) as dag:
-    nb_op = notebook_operator(
+    nb_op = python_operator(
         dag=dag,
         name="ventetidsindikatoren",
         repo="navikt/poao-ventetid",
-        nb_path="notebooks/ventetid_dvh_raw_data/fetch_raw_dvh_sky.ipynb",
+        script_path="python/test.py",
+        #nb_path="notebooks/ventetid_dvh_raw_data/fetch_raw_dvh_sky.ipynb",
         #requirements_path="requirements.txt",
         #slack_channel="{{ var.value.get('SLACK_ALERT_CHANNEL') }}",
     )
