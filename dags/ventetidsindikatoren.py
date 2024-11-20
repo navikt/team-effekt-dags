f03b614e5a46c04d1e037eea2c513e580f0b0f0c
Rigmor Lindberg <56433785+RigmorLindberg@users.noreply.github.com>
2024-11-20 12:59:39 +0100

    Update ventetidsindikatoren.py

--- dags/ventetidsindikatoren.py
+++ dags/ventetidsindikatoren.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from dataverk_airflow import notebook_operator, python_operator
+from airflow.models import Variable
 
 
 with DAG('ventetidsindikatoren', start_date=days_ago(1), schedule="0 8 * * 1-5", catchup=False) as dag:
@@ -12,5 +13,5 @@ with DAG('ventetidsindikatoren', start_date=days_ago(1), schedule="0 8 * * 1-5",
         #script_path="python/test.py",
         nb_path="notebooks/ventetid_dvh_raw_data/fetch_raw_dvh_sky.ipynb",
         requirements_path="requirements.txt",
-        slack_channel="{{ var.value.get('SLACK_ALERT_CHANNEL') }}",
+        slack_channel=Variable.get("SLACK_ALERT_CHANNEL"),
     )
