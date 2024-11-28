a0538474f2abb41821d1c8d063f2d230f78cfbf4
Rigmor Lindberg <56433785+RigmorLindberg@users.noreply.github.com>
2024-11-28 12:31:46 +0100

    Lagt til quarto operator

--- dags/ventetidsindikatoren.py
+++ dags/ventetidsindikatoren.py
@@ -20,8 +20,31 @@ with DAG('ventetidsindikatoren',
          retries=0,
          slack_channel= Variable.get('SLACK_ALERT_CHANNEL'),
          use_uv_pip_install=True,
+    ),
+    quarto_op = quarto_operator(
+        dag=dag,
+        name="MVP_datafortelling",
+        repo="navikt/poao-ventetid",
+        python_version="3.10",
+        quarto={
+            "path": "notebooks/ventetid_dvh_raw_data/ventetid_MVP.ipynb",
+            "env": "prod",
+            "id": "1c513c0b-bf77-4599-97d1-74be17a4a9b3",
+            "token": Variable.get('NADA_TOKEN'),
+        },
+        branch="master",
+        requirements_path="requirements.txt",
+        slack_channel= Variable.get('SLACK_ALERT_CHANNEL'),
+        use_uv_pip_install=True,
+        allowlist=["dm08-scan.adeo.no:1521"],
+        retries=0,
+        resources=client.V1ResourceRequirements(
+            requests={"memory": "10G", "cpu": "1500m"},
+            limits={"memory": "15G", "cpu": "2000m"},
+        ),
+        startup_timeout_seconds=600,
     )
 
     # dependencies
                   
-    # py_op >> nb_op
+    py_op >> quarto_op
