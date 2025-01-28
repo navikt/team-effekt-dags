dcd28f7962cd2607ec43fa4d7fc8b5227b9354bc
Sara Elise Wøllo <47059658+saraew@users.noreply.github.com>
2025-01-28 14:32:10 +0100

    Update test_dag.py

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -30,7 +30,7 @@ with DAG('test_dag', start_date=days_ago(1), schedule="15 8 * * 1-5", catchup=Fa
         requirements_path="requirements.txt",
         #slack_channel= Variable.get('SLACK_ALERT_CHANNEL'),
         use_uv_pip_install=True,
-        allowlist=["dm08-scan.adeo.no:1521", "datamarkedsplassen.ansatt.dev.nav.no:443"],
+        allowlist=["dm08-scan.adeo.no:1521", "data.ansatt.dev.nav.no:443"],
         retries=0,
         resources=client.V1ResourceRequirements(
             requests={"memory": "10G", "cpu": "1500m"},
