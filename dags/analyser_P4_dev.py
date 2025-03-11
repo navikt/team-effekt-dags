8b1a334851325d0392c3767d1e61fee49a4f0801
Sara Elise Wøllo <47059658+saraew@users.noreply.github.com>
2025-03-11 12:24:34 +0100

    Update analyser_P4_dev.py
    setter port eksplisitt

--- dags/analyser_P4_dev.py
+++ dags/analyser_P4_dev.py
@@ -26,7 +26,7 @@ with DAG('analyser_P4_dev', start_date=days_ago(1), schedule="15 8 * * 1-5", cat
         requirements_path="requirements.txt",
         slack_channel= Variable.get('SLACK_ALERT_CHANNEL'),
         use_uv_pip_install=True,
-        allowlist=["dmv09-scan.adeo.no:1521", "datamarkedsplassen.intern.dev.nav.no"],
+        allowlist=["dmv09-scan.adeo.no:1521", "datamarkedsplassen.intern.dev.nav.no:443"],
         retries=0,
         resources=client.V1ResourceRequirements(
             requests={"memory": "10G", "cpu": "1500m"},
