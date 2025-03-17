4281dd7932fcb1a72c1f01b7590925bddbd13f51
Sara Elise Wøllo <47059658+saraew@users.noreply.github.com>
2025-03-17 13:11:20 +0100

    Update test_dag.py
    endrer navn på DAG-en

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -11,10 +11,10 @@ from kubernetes import client
 
 
 
-with DAG('analyser_P4_dev', start_date=days_ago(1), schedule="15 8 * * 1-5", catchup=False) as dag:
+with DAG('test_dag', start_date=days_ago(1), schedule="15 8 * * 1-5", catchup=False) as dag:
     quarto_op = quarto_operator(
         dag=dag,
-        name="Analyser_Ventetid_P4",
+        name="Tester quarto book for analysen",
         repo="navikt/poao-ventetid",
         python_version="3.10",
         quarto={
