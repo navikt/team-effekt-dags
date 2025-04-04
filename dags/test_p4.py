e8d0da410aa746194f6b4c1f16ca2dba6e9d3eb6
Rigmor Lindberg <56433785+RigmorLindberg@users.noreply.github.com>
2025-04-04 12:58:26 +0200

    Create test_p4.py test av ny tidsindikator

---
+++ dags/test_p4.py
@@ -0,0 +1,41 @@
+#Tester om quarto-book-operatoren fungerer som den skal. 
+
+import os
+from airflow import DAG
+from airflow.models import Variable
+from airflow.utils.dates import days_ago
+from dataverk_airflow import quarto_operator
+from kubernetes import client
+#import logging
+
+
+
+
+with DAG('test_dag', start_date=days_ago(1), schedule="15 8 * * 1-5", catchup=False) as dag:
+    quarto_op = quarto_operator(
+        dag=dag,
+        name="Tester_ny versjon av tidsindikatoren",
+        repo="navikt/poao-ventetid",
+        python_version="3.10",
+        quarto={
+            "folder": "notebooks/ventetid_dvh_raw_data/quarto",
+            "env": "dev",
+            "id": "dcd6746c-6f14-47fc-b496-dc6c4ef54ba7",
+            "token": Variable.get("NADA_TOKEN_DEV"),
+        },
+        branch="master",
+        requirements_path="requirements.txt",
+        #slack_channel= Variable.get('SLACK_ALERT_CHANNEL'),
+        use_uv_pip_install=True,
+        allowlist=["dmv09-scan.adeo.no:1521", "datamarkedsplassen.intern.dev.nav.no:443"],
+        #retries=0,
+        resources=client.V1ResourceRequirements(
+            requests={"memory": "10G", "cpu": "1500m"},
+            limits={"memory": "15G", "cpu": "2000m"},
+        ),
+        startup_timeout_seconds=600,
+        
+    )
+
+
+quarto_op
